4eb9bfa62b8eb82e04a487dafe5b2e3ee1aae19a
haivu <vu.longhai93@gmail.com>
2021-01-04 10:20:47 +0700

    Init springboot proj

---
+++ src/test/java/com/inovationlab/businessinfo/BusinessInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inovationlab.businessinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
